0f93bf66d38bafbb46a4f9038246e7cfadd14142
selvamdhanabal <selvamdhanabal@gmail.com>
2022-05-25 16:52:55 +0530

    container app file was changed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 const App = () => {
     return <div>
-        <h1>Hello From Container!!!</h1>  
+        <h1>Hello From Container!!!!!</h1>  
         <hr />
         <MarketingApp />
     </div>
